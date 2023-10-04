16c64c7052600b565ce15215db58d711d00eef92
Abhinamol <117930506+Abhinamol@users.noreply.github.com>
2023-10-04 23:06:24 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from.import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.index,name="index"),
@@ -18,6 +19,10 @@ urlpatterns = [
     path('logout',views.logout,name="logout"),
     path('check_username_availability/', views.check_username_availability, name='check_username_availability'),
     path('check_email_availability/', views.check_email_availability, name='check_email_availability'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     
      
 ]
