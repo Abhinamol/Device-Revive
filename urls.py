f5f02803f2eb6966978e7d94631fbf4433aa75bf
Abhinamol <117930506+Abhinamol@users.noreply.github.com>
2023-10-06 15:45:45 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,23 +1,32 @@
-"""
-URL configuration for laptop project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path,include
+from django.urls import path
+from.import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-     path('', include('root.urls')),
+    path('',views.index,name="index"),
+    path('index',views.index,name="index"),
+    path('login',views.loginn,name="login"),
+    path('signup',views.signup,name="signup"),
+    path('services',views.services,name="services"),
+    path('about',views.about,name="about"),
+    path('contact',views.contact,name="contact"),
+    path('booking',views.booking,name="booking"),
+    path('why',views.why,name="why"),
+    path('desktop',views.desktop,name="desktop"),
+    path('laptop',views.laptop,name="laptop"),
+    path('userprofile',views.userprofile,name="userprofile"),
+    path('after_login',views.after_login,name="after_login"),
+    path('logout',views.logout,name="logout"),
+
+    path('check_username_availability/', views.check_username_availability, name='check_username_availability'),
+    path('check_email_availability/', views.check_email_availability, name='check_email_availability'),
+   
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+    
+    path('adminpage', views.adminpage, name='adminpage'),
+    
+     
 ]
--- views.py
+++ views.py
@@ -6,26 +6,41 @@ from django.contrib import auth
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
 from.models import Userdetails
+from django.contrib.auth.decorators import login_required
+from django.views.decorators.cache import never_cache
+
 #create your views here
+@never_cache
 def index(request):
     return render(request,'index.html')
+
 def loginn(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request, user) 
-            messages.success(request, 'Login successful!')
-            return redirect('userprofile')
+            login(request, user)
+            request.session['username'] = user.username
+            if username == 'admin' and password == 'admin':
+                
+                return redirect('adminpage')  # Redirect admin to the admin page
+            else:
+                messages.success(request, 'Login successful!')
+                return redirect('userprofile') 
+               
+           
         else:
             messages.error(request,'Invalid username or password')  # Add an error message
             return redirect('login')  # Redirect back to the login page
 
-    return render(request, 'login.html')
+    response = render(request,"login.html")
+    response['Cache-Control'] = 'no-store,must-revalidate'
+    return response
+
 
     
-    
+@never_cache    
 def signup(request):
     if request.method == "POST":
        full_name = request.POST['full-name']
@@ -67,26 +82,71 @@ def check_email_availability(request):
         data['error'] = 'Invalid request'
     return JsonResponse(data)
 
+    
 
+def adminpage(request):
+    return render(request, 'adminpage.html') 
 
+@never_cache
 def services(request):
     return render(request,'services.html')
+
+@never_cache
+@login_required(login_url='login')
 def userprofile(request):
-    return render(request,'userprofile.html')
+    if 'username' in request.session:
+        response = render(request,"userprofile.html")
+        response['Cache-Control'] = 'no-store,must-revalidate'
+        return response
+    else:
+        return redirect('login')
+
+@never_cache
 def about(request):
     return render(request,'about.html')
+
+@never_cache
 def contact(request):
     return render(request,'contact.html')
+
+@never_cache
+@login_required(login_url='login')
 def booking(request):
     return render(request,'booking.html')
+
+@never_cache
+@login_required(login_url='login')
 def desktop(request):
     return render(request,'desktop.html')
+
+@never_cache
+@login_required(login_url='login')
 def laptop(request):
     return render(request,'laptop.html')
+
+@never_cache
 def why(request):
     return render(request,'why.html')
 def after_login(request):
     return render(request,'after_login.html')
+
+@never_cache
 def logout (request):
     auth.logout(request)
     return redirect('/')
+
+def handlelogout(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return redirect('login')
+
+
+@login_required
+def userprofile(request):
+    # Get the user's name
+    username = request.user.username
+
+    # Pass the user's name to the template
+    return render(request, 'userprofile.html', {'username': username})
+
+
