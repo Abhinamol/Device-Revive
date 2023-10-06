f03a9b4b946f9e87ab0079b1de1d593798319876
Abhinamol <117930506+Abhinamol@users.noreply.github.com>
2023-10-06 15:45:12 +0530

    Add files via upload

--- settings.py
+++ settings.py
@@ -127,3 +127,17 @@ STATICFILES_DIRS=[
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'abhinamolv2024a@mca.ajce.in'
+EMAIL_HOST_PASSWORD = 'Ammu@1002'
+
+
+SESSION_ENGINE = "django.contrib.sessions.backends.db"
+SESSION_COOKIE_NAME = "sessionid"
+SESSION_COOKIE_AGE = 600
+LOGIN_URL = 'login'
